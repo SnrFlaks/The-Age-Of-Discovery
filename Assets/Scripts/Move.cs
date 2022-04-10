013582201d4da71e1f83230a864564488b86b984
Yttterbium <h3hh33hhh333hhhh3333@gmail.com>
2022-04-10 18:46:26 +0300

    Finally Move Script

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private float _speed;
+
+   private void Start()
+    {
+        _speed = 500f;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void FixedUpdate()
+    {
+        Moving();
+    }
+
+    private void Moving()
+    {
+        _rb.velocity = new Vector2(Input.GetAxis("Horizontal") * _speed * Time.fixedDeltaTime,Input.GetAxis("Vertical") * _speed * Time.fixedDeltaTime);
+    }
+    
+}
