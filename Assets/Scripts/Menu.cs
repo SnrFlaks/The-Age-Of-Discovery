d25909268f6ad5f3a1a811fff141190997de3ef0
Yttterbium <h3hh33hhh333hhhh3333@gmail.com>
2022-04-11 18:46:56 +0300

    Menu
    Added Scene menu but empty

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] public GameObject blur;
+
+    public void Exit()
+    {
+      Application.Quit();  
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
