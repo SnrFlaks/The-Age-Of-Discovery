4cea2f09813f7213bad859caaf8404da4739c713
SnrFlaks <83161350+SnrFlaks@users.noreply.github.com>
2022-04-14 20:43:51 +0300

    Add 3 Tile

--- Assets/Scripts/Buildings.cs
+++ Assets/Scripts/Buildings.cs
@@ -1,16 +1,28 @@
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Buildings : MonoBehaviour
 {
+    [SerializeField] private Vector2Int mapSizeIns;
+    private static Vector2Int mapSize;
+    private Vector2Int[][] _cellCord;
+    private string[][] _cellName;
     private Tilemap _ground;
     private Tilemap _objectInGround;
     public TileBase[] buildings;
     void Start() {
+        mapSize = mapSizeIns;
+        _cellCord = new Vector2Int[mapSize.x][];
+        _cellName = new string[mapSize.y][];
+        for (var i = 0; i < mapSize.y; i++) {
+            _cellCord[i] = new Vector2Int[mapSize.y];
+            _cellName[i] = new string[mapSize.y];
+        }
         _ground = transform.GetChild(0).GetComponent<Tilemap>();
         _objectInGround = transform.GetChild(1).GetComponent<Tilemap>();
     }
-    private void Update() 
+    private void Update()
     {
         var point = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
         var cellPosition = _ground.WorldToCell(point);
--- Assets/Scripts/BuildingsOpen.cs
+++ Assets/Scripts/BuildingsOpen.cs
@@ -1,33 +1,28 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Tilemaps;
 
 public class BuildingsOpen : MonoBehaviour
 {
-    [SerializeField] private Tilemap _buildings;
+    [SerializeField] private Tilemap buildingsInMap;
     [SerializeField] private TileBase[] buildings;
     [SerializeField] private GameObject buildMenu;
     private Text _buildName;
 
     private void Start() {
-        _buildName.text = buildMenu.transform.GetChild(0).GetComponent<Text>().text;
+        _buildName = buildMenu.transform.GetChild(0).GetComponent<Text>();
     }
 
-    void Update()
-    {
+    void Update() {
         var point = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
-        var cellP = _buildings.WorldToCell(point);
-        Debug.Log(_buildings.GetTile(cellP) + "\t" + buildings[0]);
-        if (Input.GetMouseButtonDown(0) && _buildings.GetTile(cellP) == buildings[0])
+        var cellP = buildingsInMap.WorldToCell(point);
+        Debug.Log(buildingsInMap.GetTile(cellP) + "\t" + buildings[0]);
+        if (Input.GetMouseButtonDown(0) && buildingsInMap.GetTile(cellP) == buildings[0])
         {
-            _buildName.text = "Бур";
             buildMenu.SetActive(true);
-                
+            _buildName.text = "Бур";
         }
     }
     public void Back() => buildMenu.SetActive(false);
-    
 }
