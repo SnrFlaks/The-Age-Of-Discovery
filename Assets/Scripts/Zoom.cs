60674222e2d26aea924489423bdf713642d876e9
Yttterbium <h3hh33hhh333hhhh3333@gmail.com>
2022-04-15 19:52:52 +0300

    Camera
    Camera speed depend on zoom

--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -9,21 +9,22 @@ public class Zoom : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private float sensivity;
-    
+    public static float cameraSize;
 
     private void Start()
     {
-        sensivity = 10f;
+        sensivity = 25f;
     }
 
     void Update()
     {
+        cameraSize = virtualCamera.m_Lens.OrthographicSize;
         
         if (virtualCamera.m_Lens.OrthographicSize == 1 )
         {
             if(Input.GetAxis("Mouse ScrollWheel") *  sensivity < 0){virtualCamera.m_Lens.OrthographicSize -= Input.GetAxis("Mouse ScrollWheel") * sensivity;}
         }
-        else if (virtualCamera.m_Lens.OrthographicSize == 10 )
+        else if (virtualCamera.m_Lens.OrthographicSize == 100 )
         {
             if(Input.GetAxis("Mouse ScrollWheel") *  sensivity > 0){virtualCamera.m_Lens.OrthographicSize -= Input.GetAxis("Mouse ScrollWheel") * sensivity;}
         }
@@ -31,9 +32,9 @@ public class Zoom : MonoBehaviour
         {
             virtualCamera.m_Lens.OrthographicSize = 1;
         }
-        else if (virtualCamera.m_Lens.OrthographicSize > 10)
+        else if (virtualCamera.m_Lens.OrthographicSize > 100)
         {
-            virtualCamera.m_Lens.OrthographicSize = 10;
+            virtualCamera.m_Lens.OrthographicSize = 100;
         }
         else{virtualCamera.m_Lens.OrthographicSize -= Input.GetAxis("Mouse ScrollWheel") * sensivity;}
 
