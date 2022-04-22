e298af1dc2d188057f81fddbb0c348accf5ef94a
SnrFlaks <83161350+SnrFlaks@users.noreply.github.com>
2022-04-22 19:00:12 +0300

    Add 2 ore and drill with cable

--- Assets/Scripts/Buildings.cs
+++ Assets/Scripts/Buildings.cs
@@ -8,7 +8,9 @@ public class Buildings : MonoBehaviour
     [SerializeField] private Vector2Int _mapSize;
     private string[][] _cellName;
     private int _drillCount = 1;
+    private int[][] _drillCountArr;
     private int[][] _drillMining;
+    private Vector2Int[][] _drillCord;
     private Tilemap _ground;
     private Tilemap _objectInGround;
     private TileBase[] _buildings;
@@ -22,11 +24,14 @@ public class Buildings : MonoBehaviour
         _objectInGround = transform.GetChild(1).GetComponent<Tilemap>();
         _cellName = new string[_mapSize.x][];
         _drillMining = new int[_mapSize.x][];
+        _drillCord = new Vector2Int[_mapSize.x][];
         for (var i = 0; i < _mapSize.y; i++)
         {
             _cellName[i] = new string[_mapSize.y];
             _drillMining[i] = new int[_mapSize.y];
+            _drillCord[i] = new Vector2Int[_mapSize.y];
         }
+        StartCoroutine(Mining());
     }
 
     private void Update()
@@ -41,11 +46,20 @@ public class Buildings : MonoBehaviour
                 _objectInGround.SetTile(cellPosition, _buildings[0]);
                 Debug.Log(cellPosition);
                 _cellName[cellPosition.x][cellPosition.y] = _buildings[0].name;
+                _drillCord[cellPosition.x][cellPosition.y] = new Vector2Int(cellPosition.x, cellPosition.y);
+                
             }
         }
         else if (HotBar.HotBarSelect[1])
         {
             if (_objectInGround.GetTile(cellPosition) == null) _objectInGround.SetTile(cellPosition, _buildings[1]);
+        }
+    }
+
+    IEnumerator Mining() {
+        while (true) {
+
+            yield return new WaitForSeconds(1);
         }
     }
 }
