243e3d8eac64e199d29835ed751d3480287ab377
SnrFlaks <83161350+SnrFlaks@users.noreply.github.com>
2022-04-22 20:28:23 +0300

    HotBar

--- Assets/Scripts/Buildings.cs
+++ Assets/Scripts/Buildings.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Tilemaps;
 
 public class Buildings : MonoBehaviour
 {
     [SerializeField] private Vector2Int _mapSize;
+    [SerializeField] private GameObject hotBar;
     private string[][] _cellName;
     private int _drillCount = 1;
     private int[][] _drillCountArr;
@@ -39,20 +41,23 @@ public class Buildings : MonoBehaviour
         point = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
         cellPosition = _ground.WorldToCell(point);
         if (!Input.GetMouseButtonDown(0) || HotBar.CreateLock) return;
-        if (HotBar.HotBarSelect[0])
+        for (int i = 0; i < HotBar.HotBarSelect.Length; i++)
         {
-            if (_objectInGround.GetTile(cellPosition) != _buildings[0] && (_ground.GetTile(cellPosition).name == "ironRandomTile" || _ground.GetTile(cellPosition).name == "goldRandomTile"))
+            if (HotBar.HotBarSelect[i])
             {
-                _objectInGround.SetTile(cellPosition, _buildings[0]);
-                Debug.Log(cellPosition);
-                _cellName[cellPosition.x][cellPosition.y] = _buildings[0].name;
-                _drillCord[cellPosition.x][cellPosition.y] = new Vector2Int(cellPosition.x, cellPosition.y);
-                
+                TileBase changedTile = ItemList.buildings[Array.IndexOf(ItemList.buildingsIcon, hotBar.transform.GetChild(i).GetChild(0).GetComponentInChildren<Image>().sprite)];
+                if (changedTile == _buildings[0] && _objectInGround.GetTile(cellPosition) == null && (_ground.GetTile(cellPosition).name == "ironRandomTile" || _ground.GetTile(cellPosition).name == "goldRandomTile"))
+                {
+                    _objectInGround.SetTile(cellPosition, _buildings[0]);
+                    Debug.Log(cellPosition);
+                    _cellName[cellPosition.x][cellPosition.y] = _buildings[0].name;
+                    _drillCord[cellPosition.x][cellPosition.y] = new Vector2Int(cellPosition.x, cellPosition.y);
+                }
+                if (changedTile == _buildings[1] && _objectInGround.GetTile(cellPosition) == null)
+                {
+                    _objectInGround.SetTile(cellPosition, _buildings[1]);
+                }
             }
-        }
-        else if (HotBar.HotBarSelect[1])
-        {
-            if (_objectInGround.GetTile(cellPosition) == null) _objectInGround.SetTile(cellPosition, _buildings[1]);
         }
     }
 
--- Assets/Scripts/HotBar.cs
+++ Assets/Scripts/HotBar.cs
@@ -1,13 +1,12 @@
-using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
-using UnityEngine.Tilemaps;
 
 public class HotBar : MonoBehaviour
 {
     public static bool CreateLock;
-    private TileBase[] hotBarTile;
     private Sprite[] hotBarSprite;
+    [SerializeField] private Sprite[] spriteN;
     [SerializeField] private Sprite emptySprite;
     public static readonly bool[] HotBarSelect = new bool[9];
     private void OnMouseEnter() => CreateLock = true;
@@ -17,7 +16,7 @@ public class HotBar : MonoBehaviour
     }
     private void Update() {
         for (var i = 0; i < HotBarSelect.Length; i++) {
-            transform.GetChild(i).GetChild(0).GetComponentInChildren<Image>().sprite = (hotBarSprite[i] == null) ? emptySprite : hotBarSprite[i];
+            transform.GetChild(i).GetChild(0).GetComponentInChildren<Image>().sprite = spriteN[i] == null ? emptySprite : spriteN[i];
             transform.GetChild(i).localScale = HotBarSelect[i] ? new Vector2(1.1f, 1.1f) : new Vector2(1, 1);
         }
     }
